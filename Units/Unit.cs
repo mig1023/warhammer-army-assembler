0a37e9bb09df2e130e84d3fd1bd2fc482275a6dd
mig1023 <mig1023@yandex.ru>
2019-09-01 21:16:36 +0300

    Units

---
+++ Units/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarhammerArmyAssembler.Units
+{
+    class Unit : IUnit
+    {
+        public string Name { get; }
+
+        public int Movement { get; }
+        public int WeaponSkill { get; }
+        public int BallisticSkill { get; }
+        public int Strength { get; }
+        public int Toughness { get; }
+        public int Wounds { get; }
+        public int Initiative { get; }
+        public int Attacks { get; }
+        public int Leadership { get; }
+        public int Armour { get; }
+        public int Ward { get; }
+
+        public bool HitFirst { get; }
+        public bool ImmuneToPsychology { get; }
+        public bool Stubborn { get; }
+        public bool KillingBlow { get; }
+        public bool Hate { get; }
+        public bool Regeneration { get; }
+        public bool Fear { get; }
+        public bool Terror { get; }
+        public bool Frenzy { get; }
+        public bool PoisonAttack { get; }
+        public bool Unbreakable { get; }
+        public bool ColdBlooded { get; }
+    }
+}
