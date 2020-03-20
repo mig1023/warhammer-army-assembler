9070ecff1d5a35486e31a4ea4a20778b3d6d3e9e
mig1023 <mig1023@yandex.ru>
2020-03-20 20:37:30 +0300

    test fight functions

--- WarhammerArmyAssembler/Test/TestFight.cs
+++ WarhammerArmyAssembler/Test/TestFight.cs
@@ -16,6 +16,9 @@ namespace WarhammerArmyAssembler
 
         static int lastDice = 0;
 
+        static bool attackWithKillingBlow = false;
+        static bool attackIsPoisoned = false;
+
         public static string Test(Unit unit, Unit enemy)
         {
             testConsole.Clear();
@@ -48,65 +51,259 @@ namespace WarhammerArmyAssembler
             if (!unit.Terror || enemy.Terror)
                 return;
 
-            Console("{0} try to resist of terror by {1}", enemy.Name, unit.Name);
+            Console("{0} try to resist of terror by {1} ", enemy.Name, unit.Name);
 
-            if (rollDice(DiceType.LD, unit, String.Format("{0}+", unit.Leadership), 2))
+            if (RollDice(DiceType.LD, enemy, DiceHigher(enemy.Leadership), 2))
+                Console(" --> passed");
+            else
             {
                 unit.Wounds = 0;
                 Console(" --> fail");
             }
-            else
-                Console(" --> passed");
         }
 
-        private static bool rollDice(DiceType diceType, Unit unit, string diceLine, int num = 1, int round = 2)
+        private static bool CheckInitiative(Unit unit, Unit enemy, int round)
         {
-            if ((diceType == DiceType.LD) && unit.ColdBlooded)
+            if ((round == 1) && !unit.HitFirst && !enemy.HitFirst)
             {
-                Console("cold-blooded");
-                num += 1;
+                Console("first round rule\n");
+                return true;
+            }
+            else if (unit.HitFirst && !enemy.HitFirst)
+            {
+                Console("{0} all time first\n", unit.Name);
+                return true;
+            }
+            else if (!unit.HitFirst && enemy.HitFirst)
+            {
+                Console("{0} all time first\n", enemy.Name);
+                return false;
+            }
+            else if (unit.Initiative > enemy.Initiative)
+            {
+                Console("{0} has initiative\n", unit.Name);
+                return true;
+            }
+            else if (unit.Initiative < enemy.Initiative)
+            {
+                Console("{0} has initiative\n", enemy.Name);
+                return true;
+            }
+            else
+            {
+                Console("random initiative --> ");
+
+                if (RollDice(DiceType.I, null, DiceHigher(4)))
+                {
+                    Console("{0}\n", unit.Name);
+                    return true;
+                }
+                else
+                {
+                    Console("{0}\n", enemy.Name);
+                    return false;
+                }
+            }
+        }
+
+        private static bool BreakTest(Unit unit, Unit enemy, int woundInRound)
+        {
+            Console("\n{0} break test --> ");
+
+            int temoraryLeadership = unit.Leadership;
+
+            if (unit.Stubborn)
+                Console("stubborn ");
+            else
+                temoraryLeadership -= woundInRound;
+
+            if (temoraryLeadership < 0)
+                temoraryLeadership = 0;
+            
+            if (unit.Unbreakable)
+                Console("unbreakable ");
+            else if ((enemy.Terror || enemy.Fear) && !(unit.ImmuneToPsychology || unit.Terror || unit.Fear))
+            {
+                Console("autobreak by {0} fear", enemy.Name);
+                return false;
+            }
+            else
+            {
+                if (RollDice(DiceType.LD, unit, DiceHigher(unit.Leadership), diceNum: 2))
+                {
+                    Console(" --> fail");
+                    return false;
+                }
+                else
+                    Console(" --> passed");
             }
 
-            Console("({0}, ", diceLine);
+            return true;
+        }
 
-            string diceForParse = diceLine.Replace("+", String.Empty);
+        private static bool Attack(Unit unit, Unit enemy, int round)
+        {
+            attackIsPoisoned = false;
+            attackWithKillingBlow = false;
 
-            int dice = int.Parse(diceForParse);
+            if ((unit.Wounds > 0) && (enemy.Wounds > 0))
+            {
+                Console("\n{0} --> hit ", unit.Name);
 
-            int result = rollAllDice(diceType, unit, num);
+                if (Hit(unit, enemy, round))
+                {
+                    Console(" --> wound ");
 
-            bool testPassed = testPassedByDice(result, dice, diceType);
+                    if ((PoisonedAttack(unit) || Wound(unit, enemy)) && (KillingAttack(unit) || NotAS(unit, enemy)) && (NotWard(enemy)))
+                    {
+                        Console(" --> {0} WOUND", enemy.Name);
+
+                        if (attackWithKillingBlow && enemy.IsHeroOrHisMount())
+                            enemy.Wounds = 0;
+
+                        return true;
+                    }
+                }
+                Console(" --> fail");
+            }
+            return false;
+        }
+
+        private static bool PoisonedAttack(Unit unit)
+        {
+            if (unit.PoisonAttack && (lastDice == 6))
+            {
+                attackIsPoisoned = true;
+                Console("(poison)");
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private static bool KillingAttack(Unit unit)
+        {
+            if (unit.KillingBlow && !attackIsPoisoned && (lastDice == 6))
+            {
+                attackWithKillingBlow = true;
+                Console(" --> killing blow");
+                return true;
+            }
+
+            Console(" --> AS ");
+            return false;
+        }
+
+        private static bool Hit(Unit unit, Unit enemy, int round)
+        {
+            // autohit
+
+            string chance = "4+";
+
+            if (unit.WeaponSkill > enemy.WeaponSkill)
+                chance = "3+";
+            else if ((unit.WeaponSkill * 2) < enemy.WeaponSkill)
+                chance = "5+";
+
+            return RollDice(DiceType.WS, enemy, chance, round: round);
+        }
+
+        private static bool Wound(Unit unit, Unit enemy)
+        {
+            string chance = "4+";
+
+            if (unit.Strength == (enemy.Toughness + 1))
+                chance = "3+";
+            else if (unit.Strength > (enemy.Toughness + 1))
+                chance = "2+";
+            else if ((unit.Strength + 1) == enemy.Toughness)
+                chance = "5+";
+            else if ((unit.Strength + 2) == enemy.Toughness)
+                chance = "6+";
+            else if ((unit.Strength + 2) < enemy.Toughness)
+                return false;
+
+            return RollDice(DiceType.S, enemy, chance);
+        }
+
+        private static bool NotAS(Unit unit, Unit enemy)
+        {
+            if (enemy.Armour == null)
+                return true;
+
+            int chance = unit.Strength - 3;
+
+            if (chance < 0)
+                chance = 0;
+
+            chance += enemy.Armour ?? 0;
+
+            return RollDice(DiceType.AS, enemy, DiceHigher(chance));
+        }
+
+        private static bool NotWard(Unit enemy)
+        {
+            if (enemy.Ward == null)
+                return true;
+
+            Console(" --> ward ");
+
+            return RollDice(DiceType.WARD, enemy, DiceHigher(enemy.Ward));
+        }
+
+        private static string DiceHigher(int? param)
+        {
+            return String.Format("{0}+", param);
+        }
+
+        private static bool RollDice(DiceType diceType, Unit unit, string conditionLine, int diceNum = 1, int round = 2)
+        {
+            Console("({0}, ", conditionLine);
+
+            string conditionForParse = conditionLine.Replace("+", String.Empty);
+
+            int condition = int.Parse(conditionForParse);
+
+            int result = RollAllDice(diceType, unit, diceNum);
+
+            bool testPassed = TestPassedByDice(result, condition, diceType);
 
             Console("{0}", result);
 
             if (!testPassed && unit.Hate && (diceType == DiceType.WS))      // +reroll
             {
-                result = rollAllDice(diceType, unit, num);
+                result = RollAllDice(diceType, unit, diceNum);
                 Console(", {0}", result);
             }
 
             Console(")");
 
-            return testPassedByDice(result, dice, diceType);
+            return TestPassedByDice(result, condition, diceType);
         }
 
-        private static bool testPassedByDice(int result, int dice, DiceType diceType)
+        private static bool TestPassedByDice(int result, int condition, DiceType diceType)
         {
             return (
-                ((result < dice) && (diceType == DiceType.LD))
+                ((result <= condition) && (diceType == DiceType.LD))
                 ||
-                ((result < dice) && (diceType != DiceType.LD))
+                ((result >= condition) && (diceType != DiceType.LD))
                 ||
                 ((result == 1) && (diceType != DiceType.LD))
             );
         }
 
-        private static int rollAllDice(DiceType diceType, Unit unit, int num)
+        private static int RollAllDice(DiceType diceType, Unit unit, int diceNum)
         {
             int maxRoll = 0;
             int result = 0;
 
-            for(int i = 0; i < num; i++)
+            if ((diceType == DiceType.LD) && unit.ColdBlooded)
+            {
+                Console("cold-blooded, ");
+                diceNum += 1;
+            }
+
+            for (int i = 0; i < diceNum; i++)
             {
                 int roll = rand.Next(6) + 1;
                 result += roll;
