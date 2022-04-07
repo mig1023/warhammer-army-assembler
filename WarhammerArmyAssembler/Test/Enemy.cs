a2769c09aa384d9ab9f4d27e5de677ea89b2060d
mig1023 <mig1023@yandex.ru>
2022-04-07 17:21:32 +0300

    enemy mount format fix

--- WarhammerArmyAssembler/Test/Enemy.cs
+++ WarhammerArmyAssembler/Test/Enemy.cs
@@ -10,7 +10,8 @@ namespace WarhammerArmyAssembler
 
         public Enemy(string enemyName)
         {
-            List<string> profile = enemyName.Split('/').ToList();
+            List<string> multiplesProfile = enemyName.Split(new string[] { " + " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> profile = multiplesProfile[0].Trim().Split('/').ToList();
 
             bool isSized = int.TryParse(profile[0], out int size);
 
@@ -43,6 +44,9 @@ namespace WarhammerArmyAssembler
 
             foreach (string specialRule in profile[14].Split(','))
                 this.GetType().GetProperty(specialRule.Trim()).SetValue(this, true);
+
+            if (multiplesProfile.Count > 1)
+                this.Mount = new Enemy(multiplesProfile[1].Trim());
         }
 
         private Profile NewProfile(string line) => ArmyBook.Parsers.ProfileParse(line);
@@ -110,10 +114,7 @@ namespace WarhammerArmyAssembler
             new Enemy("Griffon/The Empire/6/5/0/5/5/4/5/4/7///Terror, LargeBase"),
             new Enemy("Manticore/Chaos/6/5/0/5/5/4/5/4/5///Terror, KillingBlow, LargeBase"),
             new Enemy("Varghulf/Vampire/8/5/0/5/5/4/2/5/4///Terror, Undead, Regeneration, Hate, LargeBase"),
-            new Enemy("War Hydra/Dark Elves/6/4/0/5/5/6/2/5/6/4//Terror, LargeBase")
-            {
-                Mount = new Enemy("2/Apparentice//5/4/4/3/3/2/3/2/8///NoKillingBlow")
-            },
+            new Enemy("War Hydra/Dark Elves/6/4/0/5/5/6/2/5/6/4//Terror, LargeBase + 2/Apparentice//5/4/4/3/3/2/3/2/8///NoKillingBlow"),
             new Enemy("Stonehorn/Ogre Kingdoms/7/3/0/6/6/6/2/5/5/4//Frenzy, Terror, LargeBase"),
             new Enemy("Dragon Ogre Shaggoth/Beastmen/7/6/3/5/5/6/4/5/9/4//Terror, ImmuneToPsychology, LargeBase"),
             new Enemy("Stegadon/Lizardmen/6/3/0/5/6/5/2/4/5/4//LargeBase, ColdBlooded, Terror, Stubborn, ImmuneToPsychology")
