67e9f150f6084e42fafa0ca4e9c40d1a9f61b227
mig1023 <mig1023@yandex.ru>
2023-01-14 15:15:59 +0300

    homologue lines parsig fix

--- WarhammerArmyAssembler/ArmyBook/Load.cs
+++ WarhammerArmyAssembler/ArmyBook/Load.cs
@@ -122,7 +122,7 @@ namespace WarhammerArmyAssembler.ArmyBook
             string imagePath = String.Format("{0}\\Images\\{1}\\", Path.GetDirectoryName(xmlFileName),
                 StringParse(xmlFile.SelectSingleNode("ArmyBook/Introduction/Styles/Images/UnitsFolder")));
                 
-            string unitType = isHero ? "Heroes/Hero" : "Units/Unit";
+            string unitType = isHero ? "Heroes/*" : "Units/Unit";
             XmlNodeList xmlNodes = xmlFile.SelectNodes(String.Format("ArmyBook/Content/{0}", unitType));
 
             foreach (XmlNode xmlUnit in xmlNodes)
