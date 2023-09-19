c151152f184eeea9472c246185a8ee36ff62a2c3
mig1023 <mig1023@yandex.ru>
2023-09-19 13:57:08 +0300

    GetPropertiesDiff + properties list for units/options

--- WarhammerArmyAssembler/ArmyBook/Constants.cs
+++ WarhammerArmyAssembler/ArmyBook/Constants.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace WarhammerArmyAssembler.ArmyBook
 {
     class Constants
     {
+        public enum PropertyDiff
+        {
+            OnlyUnit,
+            OnlyOption,
+        }
+
         public static double Quarter = 0.25; 
         public static double Hulf = 0.5;
         public static int BaseArmySize = 2000;
@@ -18,7 +25,8 @@ namespace WarhammerArmyAssembler.ArmyBook
         public static Dictionary<string, string> CommonXmlOption { get; set; }
         public static Dictionary<string, string> CommonXmlSpecialRules { get; set; }
 
-        public static SortedDictionary<string, string> ProfilesNames = new SortedDictionary<string, string> {
+        public static SortedDictionary<string, string> ProfilesNames = new SortedDictionary<string, string>
+        {
             ["Movement"] = "M",
             ["WeaponSkill"] = "WS",
             ["BallisticSkill"] = "BS",
@@ -41,87 +49,137 @@ namespace WarhammerArmyAssembler.ArmyBook
             ["Rare"] = "Units",
         };
 
-        public static List<string> OptionProperties = new List<string> {
-            "Name",
-            "OnlyOneInArmy",
-            "OnlyOneSuchUnits",
-            "Multiple",
-            "Virtue",
-            "Honours",
-            "NativeArmour",
-            "Group",
-            "AutoHit",
-            "AutoWound",
-            "AutoDeath",
-            "HitFirst",
-            "HitLast",
-            "KillingBlow",
-            "ExtendedKillingBlow",
-            "HeroicKillingBlow",
-            "PoisonAttack",
-            "MultiWounds",
-            "NoArmour",
-            "NoWard",
-            "NoMultiWounds",
-            "NoKillingBlow",
-            "ArmourPiercing",
-            "Regeneration",
-            "ExtendedRegeneration",
-            "ImmuneToPsychology",
-            "ImmuneToPoison",
-            "Stubborn",
-            "Hate",
-            "Fear",
-            "Terror",
-            "Frenzy",
-            "BloodFrenzy",
-            "Unbreakable",
-            "ColdBlooded",
-            "Reroll",
-            "Stupidity",
-            "Undead",
-            "StrengthInNumbers",
-            "ImpactHit",
-            "SteamTank",
-            "Lance",
-            "Flail",
-            "ChargeStrengthBonus",
-            "Resolute",
-            "PredatoryFighter",
-            "MurderousProwess",
-            "AddToCloseCombat",
-            "Bloodroar",
-            "AddToHit",
-            "SubOpponentToHit",
-            "AddToWound",
-            "SubOpponentToWound",
-            "HitOn",
-            "OpponentHitOn",
-            "WoundOn",
-            "MasterRunic",
-            "RandomGroup",
-            "TypeUnitIncrese",
-            "WardForFirstWound",
-            "WardForLastWound",
-            "FirstWoundDiscount",
-            "NotALeader",
-            "General",
-            "MagicItemsPoints",
-            "AddToWard",
-            "AddToCast",
-            "AddToDispell",
-            "AddToWizard",
-            "AddToModelsInPack",
-            "WizardTo",
-            "MagicItems",
-            "Command",
-            "PersonifiedCommander",
-            "Mount",
-            "OnlyRuleOption",
-            "MagicResistance",
-            "Scout",
-            "Scouts",
+        public static bool IsPropertiesOnlyFor(string name, PropertyDiff diff)
+        {
+            if (!PropertiesDiff.ContainsKey(name))
+                return false;
+
+            else if (PropertiesDiff[name] != diff)
+                return false;
+
+            else
+                return true;
+        }
+
+        public static List<string> GetPropertiesDiff(PropertyDiff diff) =>
+            PropertiesDiff.Where(x => x.Value == diff).Select(x => x.Key).ToList();
+
+        private static Dictionary<string, PropertyDiff> PropertiesDiff = new Dictionary<string, PropertyDiff>
+        {
+            ["Giant"] = PropertyDiff.OnlyUnit,
+            ["HellPitAbomination"] = PropertyDiff.OnlyUnit,
+            ["ImpactHitByFront"] = PropertyDiff.OnlyUnit,
+            ["LargeBase"] = PropertyDiff.OnlyUnit,
+            ["MagicPowers"] = PropertyDiff.OnlyUnit,
+            ["MagicPowersCount"] = PropertyDiff.OnlyUnit,
+            ["MagicResistance"] = PropertyDiff.OnlyUnit,
+            ["OpponentHitOn"] = PropertyDiff.OnlyUnit,
+
+            ["AddToCast"] = PropertyDiff.OnlyOption,
+            ["AddToDispell"] = PropertyDiff.OnlyOption,
+            ["AddToModelsInPack"] = PropertyDiff.OnlyOption,
+            ["AddToWard"] = PropertyDiff.OnlyOption,
+            ["AddToWizard"] = PropertyDiff.OnlyOption,
+            ["Command"] = PropertyDiff.OnlyOption,
+            ["Group"] = PropertyDiff.OnlyOption,
+            ["MagicItems"] = PropertyDiff.OnlyOption,
+            ["MagicItemsPoints"] = PropertyDiff.OnlyOption,
+            ["MasterRunic"] = PropertyDiff.OnlyOption,
+            ["Mount"] = PropertyDiff.OnlyOption,
+            ["Multiple"] = PropertyDiff.OnlyOption,
+            ["Name"] = PropertyDiff.OnlyOption,
+            ["NativeArmour"] = PropertyDiff.OnlyOption,
+            ["OnlyOneInArmy"] = PropertyDiff.OnlyOption,
+            ["OnlyOneSuchUnits"] = PropertyDiff.OnlyOption,
+            ["OnlyRuleOption"] = PropertyDiff.OnlyOption,
+            ["PersonifiedCommander"] = PropertyDiff.OnlyOption,
+            ["RandomGroup"] = PropertyDiff.OnlyOption,
+            ["TypeUnitIncrese"] = PropertyDiff.OnlyOption,
+            ["Virtue"] = PropertyDiff.OnlyOption,
+            ["WizardTo"] = PropertyDiff.OnlyOption,
         };
+
+        //public static List<string> OptionProperties = new List<string> {
+        //    "Name",
+        //    "OnlyOneInArmy",
+        //    "OnlyOneSuchUnits",
+        //    "Multiple",
+        //    "Virtue",
+        //    "Honours",
+        //    "NativeArmour",
+        //    "Group",
+        //    "AutoHit",
+        //    "AutoWound",
+        //    "AutoDeath",
+        //    "HitFirst",
+        //    "HitLast",
+        //    "KillingBlow",
+        //    "ExtendedKillingBlow",
+        //    "HeroicKillingBlow",
+        //    "PoisonAttack",
+        //    "MultiWounds",
+        //    "NoArmour",
+        //    "NoWard",
+        //    "NoMultiWounds",
+        //    "NoKillingBlow",
+        //    "ArmourPiercing",
+        //    "Regeneration",
+        //    "ExtendedRegeneration",
+        //    "ImmuneToPsychology",
+        //    "ImmuneToPoison",
+        //    "Stubborn",
+        //    "Hate",
+        //    "Fear",
+        //    "Terror",
+        //    "Frenzy",
+        //    "BloodFrenzy",
+        //    "Unbreakable",
+        //    "ColdBlooded",
+        //    "Reroll",
+        //    "Stupidity",
+        //    "Undead",
+        //    "StrengthInNumbers",
+        //    "ImpactHit",
+        //    "SteamTank",
+        //    "Lance",
+        //    "Flail",
+        //    "ChargeStrengthBonus",
+        //    "Resolute",
+        //    "PredatoryFighter",
+        //    "MurderousProwess",
+        //    "AddToCloseCombat",
+        //    "Bloodroar",
+        //    "AddToHit",
+        //    "SubOpponentToHit",
+        //    "AddToWound",
+        //    "SubOpponentToWound",
+        //    "HitOn",
+        //    "OpponentHitOn",
+        //    "WoundOn",
+        //    "MasterRunic",
+        //    "RandomGroup",
+        //    "TypeUnitIncrese",
+        //    "WardForFirstWound",
+        //    "WardForLastWound",
+        //    "FirstWoundDiscount",
+        //    "NotALeader",
+        //    "General",
+        //    "MagicItemsPoints",
+        //    "AddToWard",
+        //    "AddToCast",
+        //    "AddToDispell",
+        //    "AddToWizard",
+        //    "AddToModelsInPack",
+        //    "WizardTo",
+        //    "MagicItems",
+        //    "Command",
+        //    "PersonifiedCommander",
+        //    "Mount",
+        //    "OnlyRuleOption",
+        //    "MagicResistance",
+        //    "Scout",
+        //    "Scouts",
+        //};
 
         public static Dictionary<int, int> ArmySizeAngles = new Dictionary<int, int>
         {
