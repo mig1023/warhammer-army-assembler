06453a15b87b7c1aec09e537f5e66a26e278d3bf
mig1023 <mig1023@yandex.ru>
2024-01-20 15:17:24 +0300

    Settings (start)

---
+++ WarhammerArmyAssembler/Settings/ConfigFile.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WarhammerArmyAssembler.Settings
+{
+    class ConfigFile
+    {
+        const string CONFIG_NAME = "WarhammerArmyAssembler.config";
+
+        public static void Save()
+        {
+            FileInfo backupConfig = new FileInfo(CONFIG_NAME);
+
+            if (File.Exists(CONFIG_NAME))
+            {
+                backupConfig.MoveTo(CONFIG_NAME + "_backup");
+            }
+
+            using (TextWriter config = new StreamWriter(CONFIG_NAME))
+            {
+                foreach (KeyValuePair<string, string> setting in Values.All())
+                    config.WriteLine($"{setting.Key} = {setting.Value}");
+            }
+        }
+
+        async public static void Load()
+        {
+            if (!File.Exists(CONFIG_NAME))
+                return;
+
+            Values.Clean();
+
+            using (FileStream config = File.OpenRead(CONFIG_NAME))
+            {
+                byte[] buffer = new byte[config.Length];
+                await config.ReadAsync(buffer, 0, buffer.Length);
+                string allSettings = Encoding.Default.GetString(buffer);
+
+                List<string> settings = allSettings
+                    .Split('\n')
+                    .ToList();
+
+                foreach (string setting in settings)
+                {
+                    List<string> parts = setting
+                        .Split('=')
+                        .Select(x => x.Trim())
+                        .ToList();
+
+                    Values.Set(parts[0], parts[1]);
+                }
+            }
+        }
+    }
+}
---
+++ WarhammerArmyAssembler/Settings/Values.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarhammerArmyAssembler.Settings
+{
+    class Values
+    {
+        private static Dictionary<string, string> Settings { get; set; }
+
+        public Values() =>
+            Clean();
+
+        public static string Get(string name)
+        {
+            if (Settings.ContainsKey(name))
+                return Settings[name];
+            else
+                return String.Empty;
+        }
+
+        public static string Set(string name, string value) =>
+            Settings[name] = value;
+
+        public static Dictionary<string, string> All() =>
+            new Dictionary<string, string>(Settings);
+
+        public static void Clean() =>
+            Settings = new Dictionary<string, string>();
+    }
+}
